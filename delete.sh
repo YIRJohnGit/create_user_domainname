d761bd9ad02e2d205e2fad7a3210ff9183498d51
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2024-03-14 14:04:01 +0530

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -5,16 +5,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-read -p "Enter the domain name: " domain
-echo -e "Verifying domain name: ${GREEN}${domain}${NC}"
-
 read -p "Enter the username: " username
 echo -e "Verifying username: ${GREEN}${username}${NC}"
 if id "$username" &>/dev/null; then
-    echo -e "${RED}Username $username exists${NC}"
+    echo -e "${YELLOW}Username $username exists${NC}"
     userdel -r "$username" # Delete the user and its home directory
-    rm -rf "/home/$domain" # Remove the folders
-    echo -e "${GREEN}User $username and associated folders deleted${NC}"
+    echo -e "${GREEN}User $username and associated home directory deleted${NC}"
 else
-    echo -e "${GREEN}Username $username does not exist${NC}"
+    echo -e "${RED}Username $username does not exist${NC}"
 fi
