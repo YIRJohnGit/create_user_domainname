93b57d12294c47832a62841062819f21985d6847
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2024-03-14 14:22:17 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,9 +3,9 @@
 # Color codes for formatting
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-# domain="ci4fwapibuild.mnserviceproviders.com"
 read -p "Enter the domain name: " domain
 
 echo -e "Verifying domain name: ${GREEN}${domain}${NC}"
@@ -20,32 +20,23 @@ read -p "Enter the username (leave empty for default): " username_input
 if [[ -z "$username_input" ]]; then
     # Set default username
     first_word=$(echo "$domain" | cut -d'.' -f1)
-    username="${first_word}"
+    random_suffix=$(openssl rand -hex 4)
+    username="${first_word}${random_suffix}"
     echo -e "${YELLOW}Default username set: ${username}${NC}"
 else
     username="$username_input"
 fi
 
-
 echo -e "Verifying username: ${GREEN}${username}${NC}"
 if id "$username" &>/dev/null; then
     echo -e "${RED}Username $username already exists${NC}"
-    echo -e "${YELLOW}Auto Suggestion of Username $username already exists${NC}"
-    random_suffix=$(openssl rand -hex 4)
-    username="${username}${random_suffix}"
-    echo -e "Generated username: ${GREEN}${username}${NC}"
+    exit 1
 else
     echo -e "${GREEN}Username $username does not exist${NC}"
 fi
 
-
-if id "$username" &>/dev/null; then
-    echo -e "${RED}Username $username exists${NC}"
-    exit 1
-else
-    echo -e "${GREEN}Username $username does not exist${NC}"
-    mkdir -p "/home/$domain/public_html" "/home/$domain/logs" "/home/$domain/email"
-    echo "$username" > "/home/$domain/$username.usr"
+mkdir -p "/home/$domain/public_html" "/home/$domain/logs" "/home/$domain/email"
+echo "$username" > "/home/$domain/$username.usr"
 echo "<?php
 date_default_timezone_set('Asia/Kolkata');
 echo 'Current date and time is ' . date('Y-m-d H:i:s');
@@ -55,6 +46,5 @@ phpinfo();
 echo "<h1>....Initiated</h1>" > "/home/$domain/public_html/index.html"
 
 useradd -d "/home/$domain" -m "$username"
-chown -R $username:$username /home/$domain
+chown -R $username:$username "/home/$domain"
 echo -e "${GREEN}User $username created${NC}"
-fi
